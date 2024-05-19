e0d01f7f807eafffb18c9a683264783bb90597e2
Jimin-Hwang00 <hwang0010@naver.com>
2024-05-19 16:56:01 +0900

    first commit

---
+++ src/main/java/ddwu/wcs/pfp/PfpApplication.java
@@ -0,0 +1,13 @@
+package ddwu.wcs.pfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddwu/wcs/pfp/PfpApplicationTests.java
@@ -0,0 +1,13 @@
+package ddwu.wcs.pfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
